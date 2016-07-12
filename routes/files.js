7385fbc0b8694503ca41d5643b3d7ff8e214ff6f
yakirn <yak.irna@gmail.com>
2016-07-12 14:10:46 +0300

    storing files in folders by date, encoding file path with base64

--- routes/files.js
+++ routes/files.js
@@ -2,12 +2,13 @@ var express = require('express');
 var multer  = require('multer');
 var storage = multer.diskStorage({
     filename: function(req, file, cb){
-        cb(null, 'aaaaaaa' + file.fieldname + '-' + Date.now());
+        cb(null, 'LinkSharing' + file.fieldname + '-' + Date.now());
     }
  })
-var upload = multer({storage: storage});
-var router = express.Router();
-var AWS = require('aws-sdk'),
+var upload = multer({storage: storage}),
+    router = express.Router(),
+    base64url = require('base64-url'),
+    AWS = require('aws-sdk'),
     fs = require('fs');
 
 router.post('/', upload.single('thefile'), function(req, res, next){
@@ -15,24 +16,30 @@ router.post('/', upload.single('thefile'), function(req, res, next){
         filePath = req.file.path;
     fs.readFile(filePath, function(err, data){
       if (err) { sendError(res, err, 'failed to read file bucket'); return;}
-
+      var storeFileName = generateStoreFileName(req.file.originalname);
       s3.putObject({
           Bucket: 'lsfirstbucket',
-          Key: req.file.filename,
+          Key: storeFileName,
           ACL: 'private',
           ServerSideEncryption: 'AES256',
           Body: data
       }, function(err, data) {
           if (err) { sendError(res, err, 'failed to put object'); return;}
-          else res.json(data)
+
+          else res.json({status: 'success', slug: base64url.encode(storeFileName)})
 
           fs.unlinkSync(filePath);
       });
 
     });
 })
+function generateStoreFileName(originalFileName){
+    var now = new Date(),
+        folder = ''.concat(now.getUTCFullYear(), '-', now.getUTCMonth()+1, '-', now.getUTCDate(), '/');
+    return folder.concat(now.getTime(), '__', originalFileName)
+}
 
-router.get('/:id', function(req, res, next) {
+router.get('/:file', function(req, res, next) {
         var d = require('domain').create()
         d.on('error', function(e){
             if(e.statusCode)
@@ -43,7 +50,7 @@ router.get('/:id', function(req, res, next) {
         })
         d.run(function(){
             var s3 = new AWS.S3(),
-                fileKey = req.params.id,
+                fileKey = base64url.decode(req.params.file),
                 params = {
                     Bucket: 'lsfirstbucket',
                     Key: fileKey
